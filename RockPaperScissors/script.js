a71d0d3660ade98c2fed87aa3c721524d3e1bed0
arpajus <arpajus@gmail.com>
2023-12-12 00:37:39 +0100

    Function game created

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -10,44 +10,60 @@ function getComputerChoice() {
     return choice;
 }
 
-function playRound(playerSelection,computerSelection){
-    let player=playerSelection.toUpperCase();
-    let computer=computerSelection.toUpperCase();
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toUpperCase();
+    let computer = computerSelection.toUpperCase();
+
     switch (player) {
         case "ROCK":
-            if (computer==="PAPER") {
-                return 0; //0 = computer wins
-            } else  if(computerSelection==="SCISSORS"){
-                return 1; // 1 = player wins
-            } else{
-                let playerWin=2; //2 = tie
+            if (computer === "PAPER") {
+                return "Computer win"; //0 = computer wins
+            } else if (computer === "SCISSORS") {
+                return "Player win"; // 1 = player wins
+            } else {
+                return "No winner"; //2 = tie
             }
             break;
         case "PAPER":
-            if (computerSelection==="PAPER") {
-                let playerWin=2;
-            } else  if(computerSelection==="SCISSORS"){
-                let playerWin=0;
-            } else{
-                let playerWin=1;
+            if (computer === "PAPER") {
+                return "No winner";
+            } else if (computer === "SCISSORS") {
+                return "Computer win";
+            } else {
+                return "Player win";
             }
             break;
         case "SCISSORS":
-            if (computerSelection==="PAPER") {
-                let playerWin=1;
-            } else  if(computerSelection==="SCISSORS"){
-                let playerWin=2;
-            } else{
-                let playerWin=0;
+            if (computer === "PAPER") {
+                return "Player win";
+            } else if (computer === "SCISSORS") {
+                return "No winner";
+            } else {
+                return "Computer win";
             }
             break;
         default:
             console.log("There is an error.")
-            let playerWin=2;
-            break;
+            return "No winner";
     }
 }
 
-let choice=getComputerChoice();
-let round=playRound("paper",choice);
-console.log(round);
+function game(){
+    let i;
+    let userPoints=0;
+    let computerPoints=0;
+    for(i=0;i<5;i++){
+        alert("We are in "+i+" round");
+        let playerSelection=prompt("Choose: Rock, Paper or Scissors");
+        let computerSelection=getComputerChoice();
+        let round=playRound(playerSelection,computerSelection);
+        alert("In this round "+ round);
+        if(round==="Computer win"){
+            computerPoints++;
+        } else{
+            userPoints++;
+        }
+        alert("After this round the points are like that: You: "+userPoints+" Computer: "+computerPoints);
+    }
+
+}
