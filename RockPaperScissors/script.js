981264a611106a67489778ddf9ce87b2597f89f3
arpajus <arpajus@gmail.com>
2023-12-13 00:47:03 +0100

    First version finished of rock paper scissors

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -1,14 +1,14 @@
+const ROCK = "Rock";
+const PAPER = "Paper";
+const SCISSORS = "Scissors";
+const ROUNDS = 5;
+
+
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3) + 1;
-    if (choice === 1) {
-        choice = "Rock";
-    } else if (choice === 2) {
-        choice = "Paper";
-    } else {
-        choice = "Scissors";
-    }
-    return choice;
+    const choices = [ROCK, PAPER, SCISSORS];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
+
 
 function playRound(playerSelection, computerSelection) {
     let player = playerSelection.toUpperCase();
@@ -48,22 +48,45 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game(){
-    let i;
-    let userPoints=0;
-    let computerPoints=0;
-    for(i=0;i<5;i++){
-        alert("We are in "+i+" round");
-        let playerSelection=prompt("Choose: Rock, Paper or Scissors");
-        let computerSelection=getComputerChoice();
-        let round=playRound(playerSelection,computerSelection);
-        alert("In this round "+ round);
-        if(round==="Computer win"){
-            computerPoints++;
-        } else{
-            userPoints++;
-        }
-        alert("After this round the points are like that: You: "+userPoints+" Computer: "+computerPoints);
+function game() {
+    let userPoints = 0;
+    let computerPoints = 0;
+    for (let i = 0; i < ROUNDS; i++) {
+        let resultRound=playRoundGame(i);
+        let update=updatePoints(resultRound,userPoints,computerPoints);
+        userPoints=update.userPoints;
+        computerPoints=update.computerPoints;
     }
-
+    sayWinner(userPoints,computerPoints);
 }
+
+function playRoundGame(numberRound){
+    alert("We are in " + (numberRound + 1) + " round");
+    let playerSelection = prompt("Choose: Rock, Paper or Scissors");
+    let computerSelection = getComputerChoice();
+    let round = playRound(playerSelection, computerSelection);
+    alert("In this round " + round);
+    return round;
+}
+
+function updatePoints(resultRound,userPoints,computerPoints){
+    if (resultRound === "Computer win") {
+        computerPoints++;
+    } else if (resultRound === "Player win") {
+        userPoints++;
+    }
+    alert("After this round the points are like that: You: " + userPoints + " Computer: " + computerPoints);
+    return{userPoints,computerPoints};
+}
+
+function sayWinner(userPoints,computerPoints){
+    if (userPoints > computerPoints) {
+        alert("You win the game!");
+    } else if (computerPoints > userPoints) {
+        alert("The computer win the game");
+    } else {
+        alert("It's a tie");
+    }
+}
+
+game();
